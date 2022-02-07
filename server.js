c7a87fb1ac043548757c85a4bb3171cd5949f8d5
juliomoura2017 <juliocesar@jcdesigner.com.br>
2022-02-07 15:12:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
 	res.status(200).send({ message: 'Ola mundo estou funcionando!' });
 })
 
+app.get('/ola', (req, res) => {
+	res.status(200).send({ message: 'Oi eu sou o Ola' });
+})
+
 app.listen(3001, () => {
 	console.log('Api rodando na porta 3001');
 })
